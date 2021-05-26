4428b3d37f503caad1eaf2ee6cbdf25e817ae036
Jonathan Tanuwijaya <jonathantw64@gmail.com>
2021-05-26 20:12:57 +0700

    added redis and session auth

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const {MONGO_USER, MONGO_PASSWORD, MONGO_PORT, MONGO_IP} = require("./config/config");
+const {
+    MONGO_USER,
+    MONGO_PASSWORD,
+    MONGO_PORT,
+    MONGO_IP,
+    REDIS_PORT,
+    REDIS_URL,
+    SESSION_SECRET
+} = require("./config/config");
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+const postRouter = require('./routes/postRoutes');
+const userRouter = require('./routes/userRoutes');
+const session = require('express-session');
+const redis = require('redis');
 
+let RedisStore = require('connect-redis')(session);
+let redisClient = redis.createClient({
+    host: REDIS_URL,
+    port: REDIS_PORT
+})
 
 const connectWithRetry = () => {
     mongoose.connect(mongoURL, {
@@ -21,10 +38,26 @@ const connectWithRetry = () => {
         });
 }
 
-const port = process.env.PORT || 3000;
 connectWithRetry();
+app.use(session({
+    store: new RedisStore({
+        client: redisClient
+    }),
+    secret: SESSION_SECRET,
+    cookie: {
+        secure: false,
+        resave: false,
+        saveUninitialized: false,
+        httpOnly: true,
+        maxAge: 30000
+    }
+}))
+app.use(express.json());
+app.use('/api/v1/posts', postRouter);
+app.use('/api/v1/users', userRouter);
+const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.send(`<h2>Hi There :)</h2>`)
+    res.send(`<h2>Hi There </h2>`)
 })
 app.listen(port, () => {
     console.log(`listening on ${port}`);
